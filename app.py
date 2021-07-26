d1d056db033d5d7f28ba742fa8750784f9c1177b
JoaoNogaroli <victhornogaroli@gmail.com>
2021-07-26 11:02:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, render_template, redirect, jsonify
 import os
+import socket
+
 app = Flask(__name__)
 port = int(os.environ.get("PORT",5000))
 
 @app.route("/", methods=["GET"])
 @app.route("/")
 def index():
-    return jsonify({'ip': request.remote_addr}),200
+    meu_nome = socket.gethostname()
+    meu_ip = socket.gethostbyname(meu_nome)
+    return jsonify({'ip': request.remote_addr,
+                'teste-ip': request.environ['REMOTE_ADDR'],
+                'teste_remote_user':request.remote_user,
+                'meu_nome': meu_nome,
+                'meu_ip': meu_ip}),200
 
-@app.route("/ip", methods=["GET"])
-def func_ip():
-    return jsonify({'teste-ip': request.environ['REMOTE_ADDR']}),200
