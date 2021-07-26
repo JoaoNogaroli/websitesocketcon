590665721652567cda457ff5b548d8c03175ddc8
JoaoNogaroli <victhornogaroli@gmail.com>
2021-07-26 10:49:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ port = int(os.environ.get("PORT",5000))
 @app.route("/")
 def index():
     return jsonify({'ip': request.remote_addr}),200
+
+@app.route("/ip", methods=["GET"])
+def func_ip():
+    return jsonify({'teste-ip': request.environ['REMOTE_ADDR']}),200
